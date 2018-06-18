b0d60aa293b01a09a76cc4f187f0dc17dcd7287d
Martin Yde Granath <my@aarhus.dk>
2018-06-18 15:23:13 +0200

    LOOP-561

---
+++ modules/loop_search_links/templates/loop_search_links_block.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * loop_search_links_block.tpl.php.
+ *
+ * Template for providing links based on subject taxonomy
+ * Colors defined in theme configuration.
+ *
+ * Available variables:
+ * - $items: An array of field values. Use render() to output them.
+ */
+?>
+
+<div class="search-filters--search-box search-filter--links">
+  <?php foreach ($items as $delta => $item): ?>
+    <div class="search-filter--link-wrapper">
+      <a style="background-color:<?php  print $item['color']; ?>" class="search-filter--link" href="<?php print $item['path']; ?>"><?php print $item['name']; ?></a>
+    </div>
+  <?php endforeach; ?>
+</div>

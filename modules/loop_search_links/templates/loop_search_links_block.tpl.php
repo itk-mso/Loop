896082b4818259a0d824e5387e6fe51c48eedfc4
Martin Yde Granath <my@aarhus.dk>
2018-06-19 20:19:38 +0200

    LOOP-602: Added quickbox and taxonomy color modules

--- modules/loop_search_links/templates/loop_search_links_block.tpl.php
+++ modules/loop_search_links/templates/loop_search_links_block.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * loop_search_links_block.tpl.php.
+ * Loop_search_links_block.tpl.php.
  *
  * Template for providing links based on subject taxonomy
  * Colors defined in theme configuration.
@@ -11,8 +11,8 @@
  * - $items: An array of field values. Use render() to output them.
  */
 ?>
-<div class="search-filters--search-box search-filter--links">
-  <div><label><?php print t('Show content from'); ?></label></div>
+<div class="search-filters--search-box search-filter--links no-horizontal-padding">
+  <div><label class="search-filter--label"><?php print t('Show content from'); ?></label></div>
   <?php foreach ($items as $delta => $item): ?>
     <div class="search-filter--link-wrapper">
       <a style="background-color:<?php  print $item['color']; ?>" class="search-filter--link" href="<?php print $item['path']; ?>"><?php print $item['name']; ?></a>

37dee23576c801bed4d3b1b8daa156a65d045e6b
Jesper Kristensen <cableman@linuxdev.dk>
2018-09-28 09:07:13 +0200

    LOOP-614: Close search links on search

--- modules/loop_search_links/js/loop_toggle_links.js
+++ modules/loop_search_links/js/loop_toggle_links.js
@@ -5,10 +5,34 @@
  * https://www.drupal.org/project/drupal/issues/2395065
  */
 (function ($) {
-  "use strict";
+  'use strict';
+
   $(document).ready(function($) {
-    $(".js-toggle-links").click(function () {
-      $(".search-filter--links").toggleClass("open");
-    })
+    let $links = $('.search-filter--links');
+    let $delegate = $('.typeahead-block--wrapper');
+
+    $('.js-toggle-links').click(function () {
+      $links.toggleClass('open');
+    });
+
+    // Attached to this outer block as the inner block is angular and throws
+    // the event handler when it has been loaded.
+    $delegate.click(function (e) {
+      let target = $(e.target) || $(e.srcElement);
+      if (target.hasClass('search-box-block--button')) {
+        $links.removeClass('open');
+      }
+    });
+
+    // Listen for enter in the search field.
+    $delegate.keypress(function (e) {
+      if (e.key === 'Enter') {
+        let target = $(e.target) || $(e.srcElement);
+        if (target.is('#loop-search-field')) {
+          $links.removeClass('open');
+        }
+      }
+    });
+
   });
 })(jQuery);

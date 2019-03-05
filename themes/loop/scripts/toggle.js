be30211ba661c3fec3becde99f08c2e37551e780
Martin Yde Granath <my@aarhus.dk>
2019-03-05 14:22:16 +0100

    Changed module and styling of loop_editorial_office_block

--- themes/loop/scripts/toggle.js
+++ themes/loop/scripts/toggle.js
@@ -3,6 +3,10 @@
     $(".js-toggle").click(function(){
       $(this).toggleClass("is-shown");
       $(".js-toggle-data").toggleClass("is-shown");
-    })
+    });
+    $(".js-toggle-notify").click(function(){
+      $(this).toggleClass("is-shown");
+      $(".js-toggle-data-notify").toggleClass("is-shown");
+    });
   });
 })(jQuery);

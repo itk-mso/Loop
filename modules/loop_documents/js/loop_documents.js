8ce5f3ddb7945bdd0d6bb41899d69b69e2578492
Mikkel Ricky <rimi@aarhus.dk>
2020-10-23 11:54:40 +0200

    DEVSUPP-343: Open external quickbox links in new tab

--- modules/loop_documents/js/loop_documents.js
+++ modules/loop_documents/js/loop_documents.js
@@ -1,7 +1,7 @@
 (function($) {
-    // Add target="_blank" to all external links in body field.
+    // Add target="_blank" to all external links in quickbox and body field.
     $(document).ready(function() {
-        $('.loop-documents--content .field-name-body a').each(function(index, el) {
+        $('.loop-documents--content .field-name-body a, .loop-documents--content .quickbox a').each(function(index, el) {
             if (/^https?:\/\//.test($(el).attr('href'))) {
                 $(el).attr('target', '_blank');
             }
